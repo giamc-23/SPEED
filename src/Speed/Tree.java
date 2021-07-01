30a57a0f0f2e587419808277d325a7e4fd6fe12f
giamc-23 <84815818+giamc-23@users.noreply.github.com>
2021-07-01 10:03:02 -0400

    Update Tree.java

--- src/Speed/Tree.java
+++ src/Speed/Tree.java
@@ -8,108 +8,39 @@ import java.util.Set;
 
 public class Tree {
 
-	private static final String OPEN_BRACKET = "{";
-	private static final String CLOSED_BRACKET = "}";
-	private static final String EMPTY = "";
-	ArrayList<Character> List;
-	String window = "";
-
-
 	public TreeNode root;
-	public ArrayList<String> EpisodeList = new ArrayList<String>();
-
 	public Tree() {
 		// Marking root event as ' ' empty
 		this.root = new TreeNode(' ');
 	}
 	public void addEvents(String events) {
+		//System.out.println("Adding event :" + events);
 		TreeNode cur_node = this.root;
-		cur_node.frequency++;
 		for(char event : events.toCharArray()) {
 			if(!cur_node.children.containsKey(event)) 
 				cur_node.children.put(event, new TreeNode(event));
-			System.out.println("The children of "+cur_node.ToString() +" are/is " +cur_node.getChildren(cur_node.event));
+			//System.out.println("The children of "+cur_node.ToString() +" are/is " +cur_node.getChildren(cur_node.event));
 			cur_node = cur_node.children.get(event);
-			cur_node.frequency++;
+			//cur_node.frequency++;
+			//cur_node.getFreq(cur_node);
+		}
+		cur_node.frequency++;
+	}
+
+	public void getFreqevents(String events){
+		TreeNode cur_node = this.root;
+		System.out.println("The frequencies for "+ events);
+		for(char event : events.toCharArray()) {
+			cur_node = cur_node.children.get(event);
+			if (cur_node == null) {
+				System.out.println("The Frequency of "+event+" is 0");
+				break;
+			}
 			cur_node.getFreq(cur_node);
-			}
-		}
-
-	public void Seperate(String seq){
-		char[] seqList = seq.toCharArray();
-		char firstletter = seqList[0];
-		char Opposite;
-		if(firstletter>='A' && firstletter<='Z'){//if it is true ,display upper case
-			Opposite = Character.toLowerCase(firstletter);
-
-		}
-		else{//if it is true ,display lower case
-			Opposite = Character.toUpperCase(firstletter);
-		}
-		for(int i = 1; i <seqList.length; i++) {
-			window += seqList[i];
-		}
-		System.out.println(window);
-
-	}
-
-	public void SPEED(String seq){
-		int max_episode_length = 1;
-		String Episode = "";
-		char Opposite;
-		String Window = "";
-
-		for(Character v: seq.toCharArray()){
-			Window += v;
-			if(v>='A' && v<='Z'){//if it is true ,display upper case
-				Opposite = Character.toLowerCase(v);
-			}
-			else{//if it is true ,display lower case
-				Opposite = Character.toUpperCase(v);
-			}
-			for(int i = 0; i < Window.length(); i++) {
-				char[] WindList = Window.toCharArray();
-				if (WindList[i] == Opposite) {
-					Episode = Window.substring(i, Window.length());
-					if (Episode.length() > max_episode_length) {
-						max_episode_length = Episode.length();
-					}
-					Window = Window.substring(Window.length() - max_episode_length, Window.length());
-					Read(Episode, 0);
-					for(String event: EpisodeList) {
-						addEvents(event);
-					}
-				}
-
-
-			}
-
 		}
 	}
 
-	public void Read(String Episode, int i){
-		if (i == Episode.length()) {
-			return;
-		}
-
-		// consider each substring `S[i, j]`
-		for (int j = Episode.length() - 1; j >= i; j--)
-		{
-
-			EpisodeList.add(Episode.substring(i,j+1));
-
-			// append the substring to the result and recur with an index of
-			// the next character to be processed and the result string
-			Read(Episode, j + 1);
-		}
-
-		Set<String> set = new HashSet<>(EpisodeList);
-		EpisodeList.clear();
-		EpisodeList.addAll(set);
-
-	}
-
-	}
+}
 
 
 
