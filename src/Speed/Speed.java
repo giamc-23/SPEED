37fdf417d1eac4b3dffb5158b6d6d0add8734271
giamc-23 <84815818+giamc-23@users.noreply.github.com>
2021-07-09 13:34:13 -0400

    Update Speed.java

--- src/Speed/Speed.java
+++ src/Speed/Speed.java
@@ -1,8 +1,7 @@
-package Speed;
-
 import java.awt.*;
 import java.util.*;
 import java.lang.Character;
+
 
 public class Speed {
 	public Tree tree;
@@ -13,23 +12,27 @@ public class Speed {
 	public static void main(String[] args) {
 		Speed speed = new Speed();
 		//speed.Read("ABbDCcaBCbdcADaBAdab", 0);
-		
-		String sequence = "CBcbAaAaAaAaAaAaAaAaAaAaAaAaAaAaAa";
+
+		String training = "ABbDCcaBCbdcADaBAdab";
+		String testing  = "ABbDCcaBCbdcADaBAdab";
 		// Get the sequence from SHDU
-		speed.run(sequence);
-		
+		speed.run(training, false);
+		speed.run(testing, true);
+
 		//speed.tree.getFreqevents("Adac");
 		//System.out.println(speed.getProbability(speed.tree,"Ada", 'b'));
 		//System.out.println(speed.getProbability(speed.tree,"Ada", 'c'));
+		//System.out.println(speed.getProbability(speed.tree, "AB", 'D'));
 		//speed.CalcProb("Ada", 'c');
+		//speed.Prediction(sequence);
 	}
 
-	public void run(String seq){
+	public void Prediction(String seq){
 		int max_episode_length = 1;
 		String Episode = "Not found";
 		char E;
 		String Window = "";
-		System.out.println("Speed starts!");
+		System.out.println("Prediction starts!");
 		char predictedEvent = ' ';
 		double maxProb = -1;
 		int successCount = 0;
@@ -44,7 +47,7 @@ public class Speed {
 				E = Character.toLowerCase(e); //if it is true ,display upper case
 			else
 				E = Character.toUpperCase(e); //if it is true ,display lower case
-			
+
 			// Episode extraction: find the episode
 			for(int i = 0; i < Window.length(); i++) {
 				char[] WindList = Window.toCharArray();
@@ -52,13 +55,79 @@ public class Speed {
 					Episode = Window.substring(i, Window.length());
 					if (Episode.length() > max_episode_length)
 						max_episode_length = Episode.length();
-					
+
 					Window = Window.substring(Window.length() - max_episode_length, Window.length());
-					
+
 					Read(Episode, 0);
 					System.out.print("All possible contexts : ");
 					for(String context: EpisodeList) {
 						if(context.length() <= max_episode_length) {
+							System.out.print(context + ",");
+						}
+					}
+					System.out.println();
+					EpisodeList.clear();
+					break;
+				}
+			}
+
+			System.out.println("Window after episode extraction : " + Window);
+
+			// use this window and the decision tree to iterate over all the possible events
+			// pick the event which has the highest probability value
+			//predictedEvent = ' ';
+			//maxProb = -1;
+			for(Character event : tree.root.children.keySet()) {
+				double prob = getProbability(tree, Window, event);
+				if(prob > maxProb) {
+					predictedEvent = event;
+					maxProb = prob;
+				}
+			}
+		}
+
+		System.out.println("Accuracy of speed " + successCount / totalAttemps);
+
+	}
+
+
+
+	public void run(String seq, boolean test){
+		int max_episode_length = 1;
+		String Episode = "Not found";
+		char E;
+		String Window = "";
+		System.out.println("Speed starts!");
+		char predictedEvent = ' ';
+		double maxProb = -1;
+		int successCount = 0;
+		int totalAttemps = 0;
+		for(Character e: seq.toCharArray()) {
+			if (e == predictedEvent)
+				successCount++;
+			totalAttemps++;
+			//System.out.println("Episode: " + Episode);
+			Window += e;
+			if (e >= 'A' && e <= 'Z')
+				E = Character.toLowerCase(e); //if it is true ,display upper case
+			else
+				E = Character.toUpperCase(e); //if it is true ,display lower case
+
+			// Episode extraction: find the episode
+			for (int i = 0; i < Window.length(); i++) {
+				char[] WindList = Window.toCharArray();
+				if (WindList[i] == E) {
+					Episode = Window.substring(i, Window.length());
+					if (Episode.length() > max_episode_length)
+						max_episode_length = Episode.length();
+
+					Window = Window.substring(Window.length() - max_episode_length, Window.length());
+
+					//System.out.println(Episode);
+					Read(Episode, 0);
+					System.out.print("All possible contexts : ");
+					for (String context : EpisodeList) {
+						if (context.length() <= max_episode_length) {
 							System.out.print(context + ",");
 							tree.addEvents(context);
 						}
@@ -70,28 +139,31 @@ public class Speed {
 			}
 
 			System.out.println("Window after episode extraction : " + Window);
-			
+
 			// Update root frequency
 			int root_freq = 0;
-			for(char c : tree.root.children.keySet()) 
+			for (char c : tree.root.children.keySet())
 				root_freq += tree.root.children.get(c).frequency;
 			tree.root.frequency = root_freq;
-			
-			
+
+
 			// use this window and the decision tree to iterate over all the possible events
 			// pick the event which has the highest probability value
-			predictedEvent = ' ';
-			maxProb = -1;
-			for(Character event : tree.root.children.keySet()) {
-				double prob = getProbability(tree, Window, event);
-				if(prob > maxProb) {
-					predictedEvent = event;
-					maxProb = prob;
+			if (test == true) {
+				predictedEvent = ' ';
+				maxProb = -1;
+				for (char event : tree.root.children.keySet()) {
+					double prob = getProbability(tree, Window, event);
+					if (prob > maxProb) {
+						predictedEvent = event;
+						maxProb = prob;
+					}
 				}
 			}
 		}
-		
-		System.out.println("Accuracy of speed " + successCount / totalAttemps);
+		if(test == true) {
+			System.out.println("Accuracy of speed " + successCount / totalAttemps);
+		}
 	}
 
 	public double getProbability(Tree tree, String window, char event) {
@@ -105,7 +177,7 @@ public class Speed {
 			else
 				break;
 		}
-		
+
 		while(!queue.isEmpty()) {
 			TreeNode cur_node = queue.poll();
 			double parent_freq = (double) cur_node.frequency;
@@ -115,36 +187,42 @@ public class Speed {
 			double all_child_freq = 0.0;
 			for(char child : cur_node.children.keySet())
 				all_child_freq += cur_node.children.get(child).frequency;
-			
+
 			double null_freq = parent_freq - all_child_freq;
 			double null_prob = null_freq/parent_freq;
 			prob = event_prob + (null_prob*prob);
 		}
 		return prob;
 	}
-	
+
 	public void CalcProb(String window, char c){
 		TreeNode cur_node = tree.root;
+		double all_child_freq = 0;
 		char[] WindList = window.toCharArray();
-		float generalp = tree.root.children.get(c).frequency;
-		float Calc = generalp/38;
-		float nullc = cur_node.children.get(WindList[0]).frequency;
-		nullc = 1/nullc;
+		double generalp = tree.root.children.get(c).frequency;
+		double Calc = generalp/tree.root.frequency;
+		double nullc = cur_node.children.get(WindList[0]).frequency;
+		for(char child : cur_node.children.keySet())
+			all_child_freq += cur_node.children.get(child).frequency;
+		nullc = nullc/all_child_freq;
 		Calc = nullc * Calc;
 		cur_node = cur_node.children.get(WindList[0]);
 		TreeNode p = cur_node.children.get(c);
-		float pfreq;
+		double pfreq;
 		if (p == null){
 			pfreq = 0;
 		} else{
 			pfreq = p.frequency;
 		}
-		pfreq = pfreq*nullc;
+		pfreq = pfreq/cur_node.frequency;
 		Calc += pfreq;
 		for(int i = 1; i < window.length(); i++) {
+			all_child_freq = 0;
 			cur_node = cur_node.children.get(WindList[i]);
 			nullc = cur_node.frequency;
-			nullc = 1/nullc;
+			for(char child : cur_node.children.keySet())
+				all_child_freq += cur_node.children.get(child).frequency;
+			nullc = nullc/ all_child_freq;
 			Calc = nullc * Calc;
 			p = cur_node.children.get(c);
 			if (p == null){
